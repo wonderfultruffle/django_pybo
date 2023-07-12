b2cd97fd11faa178978f80da180eed309731d085
kjh <jh2two@gmail.com>
2023-07-12 23:36:39 +0900

    jump-to-django_210~301

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question
+from django.http import HttpResponseNotAllowed
 from django.utils import timezone
+from .models import Question
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
@@ -16,5 +18,31 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get("content"), create_date=timezone.now())
-    return redirect("pybo:detail", question_id)
+    # question.answer_set.create(content=request.POST.get("content"), create_date=timezone.now())
+    # return redirect("pybo:detail", question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect("pybo:detail", question_id=question_id)
+    else:
+        return HttpResponseNotAllowed("Only POST is passible.")
+    context= {'question': question, 'form':form}
+    return render(request, "pybo/question_detail.html", context)
+
+def question_create(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+        
