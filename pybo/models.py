1418f10cc15b1dad9e731d53edf84f7466dd5e2f
kjh <jh2two@gmail.com>
2023-07-24 22:20:00 +0900

    jump-to-django_307:model+login

--- pybo/models.py
+++ pybo/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField()
 
     def __str__(self):
@@ -12,4 +14,5 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField()
--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotAllowed
 from django.utils import timezone
-from django.core.paginator import Paginator
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -22,6 +23,7 @@ def detail(request, question_id):
     context = {"question": question}
     return render(request, "pybo/question_detail.html", context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     # question.answer_set.create(content=request.POST.get("content"), create_date=timezone.now())
@@ -30,20 +32,24 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect("pybo:detail", question_id=question_id)
     else:
-        return HttpResponseNotAllowed("Only POST is passible.")
+        # return HttpResponseNotAllowed("Only POST is passible.")
+        form = AnswerForm()
     context= {'question': question, 'form':form}
     return render(request, "pybo/question_detail.html", context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
