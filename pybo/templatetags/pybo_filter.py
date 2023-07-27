85fd7f0a5d125f43e069643ee69d2540cf779883
kjh <jh2two@gmail.com>
2023-07-28 00:00:15 +0900

    jump-to-django_313: markdown

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,7 @@
+
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -9,3 +12,8 @@ def sub(value, arg):
 @register.filter
 def div(value, arg):
     return ceil(value/arg)
+
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
