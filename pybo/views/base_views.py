a47d67b61835127dda86bd0feafa5935bd683f12
kjh <jh2two@gmail.com>
2023-07-28 22:45:16 +0900

    jump-to-django_314: search

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from ..models import Question
@@ -5,11 +6,21 @@ from ..models import Question
 # Create your views here.
 def index(request):
     page = request.GET.get("page", "1")
-    print(page)
+    kw = request.GET.get("kw", '')
     question_list = Question.objects.order_by("-create_date")
+    
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) | 
+            Q(content__icontains=kw)| 
+            Q(answer__content__icontains=kw) | 
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)).distinct()
+    
     paingnator = Paginator(question_list, 10)
     page_obj = paingnator.get_page(page)
-    context = {"question_list": page_obj}
+    
+    context = {"question_list": page_obj, "page":page, "kw": kw}
     return render(request, "pybo/question_list.html", context)
 
 def detail(request, question_id):
